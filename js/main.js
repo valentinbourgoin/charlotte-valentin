b5fbd8f037c6992571556fc14920a3cbb4ccc02c
Valentin <valentin.bourgoin@runningheroes.com>
2018-02-25 13:18:09 +0100

    Add infos section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-function initMap() {
-  var uluru = {lat: 47.749812, lng: -3.091012};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
+// $(function(){
+//   var stickyHeaderTop = $('.landing').width();
+
+//   $(window).scroll(function(){
+//     console.log($(window).scrollTop());
+//     if( $(window).scrollTop() > stickyHeaderTop ) {
+//       $('.menu').addClass('menu--fixed');
+//     } else {
+//       $('.menu').removeClass('menu--fixed');
+//     }
+//   });
+// });
